157cc411e3ca717cffeed3cc6225437a4dc5a304
XilliX <k17549832@gmail.com>
2026-09-22 15:54:09 +0300

    Добавить первую версию сайта: src/data/accounts.js

---
+++ src/data/accounts.js
@@ -0,0 +1,14 @@
+export const accounts = [
+  {
+    login: "admin",
+    password: "admin",
+    name: "ВЛАД",
+    role: "admin",
+  },
+  {
+    login: "student",
+    password: "student",
+    name: "ВЛАД",
+    role: "student",
+  },
+];
